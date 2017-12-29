643f0675d1c21e334f63b4e4216373e58f24f775
Unknown <gregheff@gmail.com>
2017-12-29 11:15:04 -0500

    turning and waypoints implemented

--- Assets/Scripts CMD/CombatCommandMove.cs
+++ Assets/Scripts CMD/CombatCommandMove.cs
@@ -2,62 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CombatCommandMove : CombatCommand {
 
-	NavPointCreator nav;
-	public bool moving;
-	public List<Vector3> navPoint;	
+public class CombatCommandMove : CombatCommand
+{
+    NavPointCreator nav;
+    public List<Vector3> navPoint;
 
+    public bool isPaused = false;
+    public bool moving = false;
 
-	public override void Start()
-	{
+    public override void Start()
+    {
+        nav = (NavPointCreator)gameObject.AddComponent<NavPointCreator>();
+        nav.DistanceSet += RoundDistance;
+    }
 
-		nav = (NavPointCreator)gameObject.AddComponent<NavPointCreator>();
-		nav.DistanceSet += RoundDistance; 
-	}
+    IEnumerator MoveToWaypoint()
+    {
+        for (int i = 0; i < navPoint.Count; i++)
+        {
+            Vector3 targetPosition = new Vector3(
+                                        navPoint[i].x,
+                                        gameObject.transform.parent.position.y,
+                                        navPoint[i].z
+                                        );
+            while ((gameObject.transform.parent.position != targetPosition))
+            {
+                moving = true;
+                gameObject.transform.parent.position = Vector3.MoveTowards(gameObject.transform.parent.position,
+                                                                            targetPosition,
+                                                                            (5 * Time.deltaTime));
 
+                while (isPaused)
+                {
+                    moving = false;
+                    yield return null;
+                }
 
-	
- IEnumerator MoveToWaypoint(){
+                yield return null;
+            }
+            yield return null;
+        }
 
-	 	Transform parent;
-		parent = this.transform.parent; 
-		print("Moving to waypoint");
-			parent.position =  Vector3.MoveTowards(parent.position, navPoint[0], (3* Time.deltaTime));
-			yield return new WaitForSeconds(3);
-}
+        moving = false;
+        print("done moving");
+    }
 
+    public override void Update()
+    {
+        if (Input.GetMouseButtonDown(0))// && (!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()))
+        {
+            nav.SetNav();
+            navPoint.Add(nav.point1);
+            return;
+        }
 
+        if (Input.GetKeyDown(KeyCode.G) && (isPaused == false) && (moving == false))
+        {
+            StartCoroutine("MoveToWaypoint");
+        }
 
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPaused = !isPaused;
+        }
+    }
 
+    void RoundDistance(float d, Vector3 p)
+    {
+        slots = Mathf.RoundToInt(d);
+        print("Distance Rounded= " + slots);
+        AddToList();
+        return;
+    }
 
-	public override void Update()
-	{
-			if (Input.GetMouseButtonDown(0))// && (!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()))
-			{
-				nav.SetNav();
-				navPoint.Add(nav.point1);
-				return;
-			}	
-
-			if (moving == true)
-			{
-				//get current position
-				MoveToWaypoint();
-			}
-
-			if (Input.GetKeyDown(KeyCode.G))
-			{
-				StartCoroutine("MoveToWaypoint");
-			}
-	}
-
-	void RoundDistance(float d, Vector3 p)
-	{
-		slots = Mathf.RoundToInt(d);
-		print("Distance Rounded= " + slots);	
-		AddToList();
-		return;
-	}
+    public void UnSubscribe()
+    {
+        nav.DistanceSet -= RoundDistance;
+    }
 
 }
